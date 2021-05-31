b307298126e04ba26ea7b7a9580283b503392ea7
Jannis Blossey <jannis@blossey.eu>
2021-05-31 13:13:27 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Navbar/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Navbar,
   },
 };
 </script>
@@ -21,8 +18,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  text-align: left;
+  color: black;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <b-navbar toggleable="true" type="dark" variant="dark">
+        <b-navbar-brand href="#">DYHL Card Template</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item href="#">Empathise</b-nav-item>
+                <b-nav-item href="#">Define</b-nav-item>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
